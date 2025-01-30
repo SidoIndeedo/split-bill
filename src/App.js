9c9a7409e5f9b9e210ff29d84a0bce518ab3837f
SidoIndeedo <sidharths1001@gmail.com>
2025-01-30 18:53:39 +0530

    idflm

--- src/App.js
+++ src/App.js
@@ -24,6 +24,9 @@ export default function App() {
   return (
     <div className="app">
       <FriendsList />
+      <FormAddFriend />
+      <Button>Add Friend</Button>
+      <FormSplitBill />
     </div>
   );
 }
@@ -58,7 +61,49 @@ function Friend({ friend }) {
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even!</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑 Friend name</label>
+      <input type="text" />
+
+      <label>🌅Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰Bill Value</label>
+      <input type="text" />
+
+      <label>🦍Your expense</label>
+      <input type="text" />
+
+      <label>😼X's expense</label>
+      <input type="text" disabled />
+
+      <label>Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
   );
 }
